0ca508bf339a8c5bf7960c4b4d28f71f8500003b
SalMirabella <descendz@gmail.com>
2017-12-15 17:11:15 +0100

    Commit File

---
+++ mvcCoreOverview/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcCoreOverview.Models;
+
+namespace mvcCoreOverview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult AnotherAction()
+        {
+            return View();
+        }
+
+        public IActionResult AnotherView()
+        {
+            return View("SomeView");
+        }
+
+        public IActionResult ViewWithParamether()
+        {
+            string someString = "Pippo Baudo";
+            return View("ViewWithParamether", someString);
+        }
+
+        public IActionResult PassingData()
+        {
+            ViewBag.fruit = "Apple";
+            ViewData["Color"] = "Red";
+            TempData["Number"] = 5;
+            return View();
+        }
+
+
+
+    }
+}
